83055b1e1d92638844215032427abe3edc564c22
LauAnderson <lauranderson95@gmail.com>
2022-06-09 18:15:12 -0300

    Agregando ejercicio

---
+++ src/test/java/com/example/proyectoHolaMundoGradle/ProyectoHolaMundoGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoHolaMundoGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoHolaMundoGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
